a7e4f1080c3e03fae77eff1e841b0487b8c0061c
Salika <sxr655@student.bham.ac.uk>
2025-08-20 13:25:29 +0400

    fixed breaking changes

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -49,7 +49,7 @@ const context = await esbuild.context({
   logLevel: "info",
   sourcemap: prod ? false : "inline",
   treeShaking: true,
-  outfile: "dist/main.js",
+  outfile: "main.js",
   minify: prod,
 });
 
